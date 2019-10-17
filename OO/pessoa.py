f7497dc60f06e8cc20704bbfc35410a2ba1d4b08
engnogueira <engnogueira@hotmail.com>
2019-10-17 17:55:47 -0300

    Criação da classe Pessoa
    Estanciamento do objeto pessoa, com a impressão de uma saudação pessoal
    
    inserção da função print com a definição do ID da instância para verificação de similaridade dos instanciamentos
    
    Signed-off-by: engnogueira <engnogueira@hotmail.com>

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,9 +1,10 @@
 class Pessoa:
     def cumprimentar(self):
-        return 'Olá'
+        return f'Olá {id(self)}'
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
+    print(id(p))
     print(p.cumprimentar())
     print('teste de conexão do VS Code')
